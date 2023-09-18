cb27496abc3192653d2e9699db9215188bd8fb73
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-09-18 22:36:14 +0630

    add routes for api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    get 'text_analysis/analyze'
+    post 'text_analysis/analyze', to: 'text_analysis#analyze'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
