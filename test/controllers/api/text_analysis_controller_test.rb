8acf2520665519e180f6cc8de0c39bc01a58ed12
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-09-20 12:41:19 +0630

    fix rubocop errors

--- test/controllers/api/text_analysis_controller_test.rb
+++ test/controllers/api/text_analysis_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::TextAnalysisControllerTest < ActionDispatch::IntegrationTest
-  test "should get analyze" do
+  test 'should get analyze' do
     get api_text_analysis_analyze_url
     assert_response :success
   end
